80b1317f6341f9c8bb271007135175ca412464c2
Ingo Stramm <ingostramm@gmail.com>
2021-08-20 09:00:14 -0300

    minor fix

--- gpd.php
+++ gpd.php
@@ -4,7 +4,7 @@
  * Plugin Name: Gestão por Desempenho
  * Plugin URI: https://agencialaf.com
  * Description: Plugin dos sistema de Gestão por Desempenho.
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Ingo Stramm
  * Text Domain: gpd
  * License: GPLv2
